60c08b86528a15b9bc6ab03e99f3b3d0f74bdde6
victo <victordeng928@gmail.com>
2023-08-27 02:49:59 -0400

    F

--- app.py
+++ app.py
@@ -9,99 +9,102 @@ from flask_cors import CORS  # Import the CORS library
 app = Flask(__name__)
 CORS(app) 
 app.secret_key = "supersecretkey"
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+#openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = "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"
 
 
 history = []
 @app.route('/get_objects', methods=['POST'])  # Note the method specification
 def user_input():
     global history
-    #get user input
-    incoming_data = request.get_json()  # Get incoming JSON data
-    userInput = incoming_data.get('user_input', '') if incoming_data else ''
-    print(userInput)
-    if userInput.lower() == "restart history":
-        history = []
-        return jsonify({"message": "History restarted", "products": []})
-    
-    history.append({"role": "user", "content": userInput})
+    try:
+        #get user input
+        incoming_data = request.get_json()  # Get incoming JSON data
+        userInput = incoming_data.get('user_input', '') if incoming_data else ''
+        print(userInput)
+        if userInput.lower() == "restart history":
+            history = []
+            return jsonify({"message": "History restarted", "products": []})
+        
+        history.append({"role": "user", "content": userInput})
 
-    #get recommendation from gpt3
-    prompt1 = ('''You are a shopping assistant with a specific task: to provide improved product recommendations.
+        #get recommendation from gpt3
+        prompt1 = ('''You are a shopping assistant with a specific task: to provide improved product recommendations.
 
-After analyzing the user's input, their past requirements, and product recommended last time, your output must strictly adhere to one of the following three formats:
+    After analyzing the user's input, their past requirements, and product recommended last time, your output must strictly adhere to one of the following three formats:
 
-1) A list of 1 to 3 merchandise keywords, without any additional text or explanation, formatted as a list of strings: e.g., ["luxury red leather shoes", "designer red suede shoes", "high-end red patent leather heels"].
+    1) A list of 1 to 3 merchandise keywords, without any additional text or explanation, formatted as a list of strings: e.g., ["luxury red leather shoes", "designer red suede shoes", "high-end red patent leather heels"].
 
-OR
+    OR
 
-2) Short answer (max 2 sentences) to any explicid customer questions that begins with an asterisk '*' followed by an explanation that uses all your knowledge to answer the question about the products recommended.
+    2) Short answer (max 2 sentences) to any explicid customer questions that begins with an asterisk '*' followed by an explanation that uses all your knowledge to answer the question about the products recommended.
 
-OR
+    OR
 
-3) An error message that begins with an asterisk '*' followed by a concise error text, only when the input is extremely unclear, offensive, or adversarial.
+    3) An error message that begins with an asterisk '*' followed by a concise error text, only when the input is extremely unclear, offensive, or adversarial.
 
 
-Try your best to give relevant recommendations based on past and new requirements and product recommendations. Return an answer only to explicit questions about products recommended, and throw and error only when absolutely necessary. Do not include any other types of messages, explanations, or formats.
+    Try your best to give relevant recommendations based on past and new requirements and product recommendations. Return an answer only to explicit questions about products recommended, and throw and error only when absolutely necessary. Do not include any other types of messages, explanations, or formats.
 
-'''             
+    '''             
+            )
+
+        messages_to_send = [{"role": "system", "content": prompt1}]
+        messages_to_send.extend(history)
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages_to_send,
         )
-
-    messages_to_send = [{"role": "system", "content": prompt1}]
-    messages_to_send.extend(history)
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages_to_send,
-    )
-    ai_message = chat_completion.choices[0].message.content
-    print(ai_message)
-    if(ai_message[0] == "*"):
+        ai_message = chat_completion.choices[0].message.content
+        print(ai_message)
+        if(ai_message[0] == "*"):
+            history.append({"role": "assistant", "content": ai_message})
+            return jsonify({"message": ai_message,"products": []})
         history.append({"role": "assistant", "content": ai_message})
-        return jsonify({"message": ai_message,"products": []})
-    history.append({"role": "assistant", "content": ai_message})
-    clean_ai_message = ai_message.replace(r"(\w)'(\w)", r"\1\'\2")
-    recommendation_list= ast.literal_eval(clean_ai_message)
+        clean_ai_message = ai_message.replace(r"(\w)'(\w)", r"\1\'\2")
+        recommendation_list= ast.literal_eval(clean_ai_message)
+        
+        #get product recommendation from amazon
+        amazon_products = get_products(recommendation_list)
+        amazon_products_str = json.dumps(amazon_products)
+        prompt3 = (f'''Here is a list of amazon product information in json format:
+
+    {amazon_products_str}
+
+    You are a shopping adviser. Based on this information, analyze each product and write a short critical product evaluation for each product. Don't include the title of the product in the description, but discuss the essential features of the product.
+    write the evaluations as a Python list of strings. 
     
-    #get product recommendation from amazon
-    amazon_products = get_products(recommendation_list)
-    amazon_products_str = json.dumps(amazon_products)
-    prompt3 = (f'''Here is a list of amazon product information in json format:
 
-{amazon_products_str}
-
-You are a shopping adviser. Based on this information, analyze each product and write a short critical product evaluation for each product. Don't include the title of the product in the description, but discuss the essential features of the product.
-write the evaluations as a Python list of strings. 
- 
-
-["This is a evaluation of product1", "This is a evluation of product2", "This is a evalution of product3", etc.]
+    ["This is a evaluation of product1", "This is a evluation of product2", "This is a evalution of product3", etc.]
 
 
-The products must be in the correct order. The output must be in Python with precisely correct syntax. Do not include any other types of messages, explanations, or formats.
-''')
-    
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt3}],
-    )
-    evaluation = chat_completion.choices[0].message.content
-    
-    history.append({"role": "assistant", "content": evaluation})
-    amazon_products_list = filter_products(amazon_products,ast.literal_eval(evaluation))
-    print(amazon_products_list)
-        #get reply from gpt3
-    prompt2 =(f'''You are a shopping assistant tasked to find the best product based on the customer’s needs. Here is the list of products you can recommend from: \n {amazon_products_str} \nReturn a short message recommending one or two of the product that best fit the customer’s needs. Give only the name of the product and reason for recommendation. Limit the output to less than 100 words. \nUser input: {userInput}''')
-    chat_completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt2}],
-    )
-    reply = chat_completion.choices[0].message.content
-    history.append({"role": "assistant", "content": reply})
-    
-    result = {
-        'products': amazon_products_list,
-        'message': reply,
-    }
-    return jsonify(result)
-    """except Exception as e:
+    The products must be in the correct order. The output must be in Python with precisely correct syntax. Do not include any other types of messages, explanations, or formats.
+    ''')
+        
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt3}],
+        )
+        evaluation = chat_completion.choices[0].message.content
+        
+        history.append({"role": "assistant", "content": evaluation})
+        amazon_products_list = filter_products(amazon_products,ast.literal_eval(evaluation))
+        print(amazon_products_list)
+            #get reply from gpt3
+        prompt2 =(f'''You are a shopping assistant tasked to find the best product based on the customer’s needs. Here is the list of products you can recommend from: \n {amazon_products_str} \nReturn a short message recommending one or two of the product that best fit the customer’s needs. Give only the name of the product and reason for recommendation. Limit the output to less than 100 words. \nUser input: {userInput}''')
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt2}],
+        )
+        reply = chat_completion.choices[0].message.content
+        history.append({"role": "assistant", "content": reply})
+        
+        result = {
+            'products': amazon_products_list,
+            'message': reply,
+        }
+        return jsonify(result)
+    except Exception as e:
         print(e)
-        return jsonify({"message": "Something went wrong"})"""
+        history = []
+        return jsonify({"message": "Error Occurs. Please enter your needs again", "products": []})
 
 def filter_products(products, descriptions):
     filtered_products = []
